338d3894e155412621df81736ff24ae1548f8410
Luigi <luigi.derussis@polito.it>
2015-04-13 14:56:52 +0200

    fix a typo

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 '''
 Created on Apr 10, 2015
 @author: Luigi De Russis <luigi.derussis@polito.it>
-Copyright (c) 2015 Dario Bonino
- 
+Copyright (c) 2015 Luigi De Russis
+
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
     http://www.apache.org/licenses/LICENSE-2.0
- 
+
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -30,6 +30,7 @@ track_list = TrackList()
 # create a player
 player = Player()
 
+
 @app.route('/')
 def index():
     return render_template('index.html', tracks=track_list.tracks)
@@ -44,7 +45,7 @@ def show(id):
 def play(id):
     # get the path of the desired track
     path = track_list.tracks[id].path
-    # play the song
+    # play the track
     player.play(path)
     return redirect(url_for('show', id=id))
 
@@ -53,6 +54,7 @@ def stop(id):
     # stop the player
     player.stop()
     return redirect(url_for('show', id=id))
+
 
 if __name__ == '__main__':
     # get the list of tracks from the given folder
